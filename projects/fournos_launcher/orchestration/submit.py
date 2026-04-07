2fab2c2a7addc1794d985b667e4cfc91df813fd1
Kevin Pouget <kpouget@redhat.com>
2026-04-07 16:31:05 +0200

    [fournos_launcher] Make it submit jobs

--- projects/fournos_launcher/orchestration/submit.py
+++ projects/fournos_launcher/orchestration/submit.py
@@ -8,26 +8,12 @@ from projects.core.library import env, config, run
 
 from projects.fournos_launcher.toolbox.submit_and_wait.main import run as submit_and_wait
 
-def filter_out_launcher_overrides():
-    """Remove the config fields that apply to FOURNOS, keep only
-    those that we want to pass to the FOURNOS job"""
-
-    overrides = config.project.get_config("overrides", {})
-    new_overrides = {}
-    for key, value in overrides.items():
-        if config.project.has_config(key):
-            continue
-        new_overrides[key] = value
-
-    config.project.set_config("overrides", new_overrides, print=False)
-
-
 def init():
     env.init()
     run.init()
     config.init(pathlib.Path(__file__).parent, apply_config_overrides=False)
     config.project.apply_config_overrides(ignore_not_found=True)
-    filter_out_launcher_overrides()
+    config.project.filter_out_used_overrides()
 
 
 def submit_job():
