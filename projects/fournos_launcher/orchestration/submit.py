3e7350c9024fd3b1541a3bd93675aa53b623f206
Kevin Pouget <kpouget@redhat.com>
2026-04-08 13:28:06 +0200

    [fournos_launcher] orchestration: Use the vault

--- projects/fournos_launcher/orchestration/submit.py
+++ projects/fournos_launcher/orchestration/submit.py
@@ -4,7 +4,7 @@ logger = logging.getLogger(__name__)
 
 import os
 
-from projects.core.library import env, config, run
+from projects.core.library import env, config, run, vault
 
 from projects.fournos_launcher.toolbox.submit_and_wait.main import run as submit_and_wait
 
@@ -14,12 +14,21 @@ def init():
     config.init(pathlib.Path(__file__).parent, apply_config_overrides=False)
     config.project.apply_config_overrides(ignore_not_found=True)
     config.project.filter_out_used_overrides()
+    vault.init(config.project.get_config("vaults"))
+
+    prepare_env()
+
+
+def prepare_env():
+    kubeconfig_path = vault.get_vault_content_path(
+        config.project.get_config("fournos.kubeconfig.vault.name"),
+        config.project.get_config("fournos.kubeconfig.vault.key")
+    )
+
+    os.environ["KUBECONFIG"] = str(kubeconfig_path)
 
 
 def submit_job():
-
-    os.environ["KUBECONFIG"] = str(pathlib.Path(os.environ["PSAP_FORGE_FOURNOS_CI_SECRET_PATH"]) / "kubeconfig")
-
     submit_and_wait(
       cluster_name=config.project.get_config("cluster.name"),
       project=config.project.get_config("ci_job.project"),
