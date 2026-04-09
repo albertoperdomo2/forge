fad34c4d1e714ae7e51cfb589c4e80b04ff69f76
Kevin Pouget <kpouget@redhat.com>
2026-04-09 17:08:30 +0200

    [fournos_launcher] orchestration: cli: add the CLI entrypoint

--- projects/fournos_launcher/orchestration/cli.py
+++ projects/fournos_launcher/orchestration/cli.py
@@ -5,6 +5,7 @@ Fournos launcher project CLI Operations
 
 import sys
 import types
+import shlex
 
 import logging
 logger = logging.getLogger(__name__)
@@ -12,6 +13,8 @@ logger = logging.getLogger(__name__)
 import click
 
 from projects.fournos_launcher.orchestration import submit as submit_mod
+from projects.core.library import config
+from projects.core.library.cli import safe_cli_command
 
 
 @click.group()
@@ -24,11 +27,56 @@ def main(ctx):
 
 
 @main.command()
+@click.option('--cluster', help='Target cluster name')
+@click.option('--project', help='Project to run (e.g., llm_d)')
+@click.option('--args', help='Arguments to pass to the project (space-separated string)')
+@click.option('--override', '-o', multiple=True, help='Config overrides in key=value format')
 @click.pass_context
-def submit(ctx):
+@safe_cli_command
+def submit(ctx, cluster, project, args, override):
     """Submit a CI job to FOURNOS CI entrypoint."""
 
-    sys.exit(submit_mod.submit_job())
+    # Parse args string into list
+    args_list = []
+    if args:
+        args_list = shlex.split(args)
+
+    # Parse overrides into dict
+    extra_overrides = {}
+    for override_str in override:
+        if '=' in override_str:
+            key, value = override_str.split('=', 1)
+            extra_overrides[key] = value
+        else:
+            logger.warning(f"Ignoring invalid override format: {override_str} (expected key=value)")
+
+    # Override config values if provided
+    if cluster:
+        config.project.set_config("cluster.name", cluster)
+    else:
+        cluster = config.project.get_config("cluster.name")
+        if not cluster:
+            raise ValueError("--cluster or cluster.name is mandatory")
+    logging.info(f"Using cluster {cluster}")
+
+    if project:
+        config.project.set_config("ci_job.project", project)
+    else:
+        project = config.project.get_config("ci_job.project")
+        if not project:
+            raise ValueError("--project or ci_job.project is mandatory")
+
+    logging.info(f"Using project {project}")
+
+    if args_list:
+        config.project.set_config("ci_job.args", args_list)
+        logging.info(f"Using args {args_list}")
+
+    if extra_overrides:
+        config.project.set_config("extra_overrides", extra_overrides)
+        logging.info(f"Using overrides {extra_overrides}")
+
+    return submit_mod.submit_job()
 
 
 if __name__ == "__main__":
