4dd6f07ce5c073a5b8ed4d66c3cb8d97a6499c20
Kevin Pouget <kpouget@redhat.com>
2026-05-19 11:23:32 +0200

    [projects] Add __init__.py files

---
+++ projects/fournos_launcher/toolbox/__init__.py
@@ -0,0 +1,6 @@
+"""
+FOURNOS Job Management Toolbox
+
+Commands for managing FOURNOS jobs including submission, monitoring, cleanup, and shutdown operations.
+FOURNOS provides batch job orchestration capabilities for running workloads in Kubernetes environments.
+"""
---
+++ projects/skeleton/toolbox/__init__.py
@@ -0,0 +1,6 @@
+"""
+Skeleton/Example Toolbox
+
+Template and example commands for creating new toolbox projects. Provides basic cluster information
+gathering capabilities and serves as a starting point for developing new toolbox functionality.
+"""
