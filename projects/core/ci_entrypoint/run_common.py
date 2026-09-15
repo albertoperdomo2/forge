63e7c34aff6922db3e18ab0b7d9bba4c73b088d4
Alberto Perdomo <aperdomo@redhat.com>
2026-09-15 10:26:31 +0100

    chore: Update the benchconf ref
    Signed-off-by: Alberto Perdomo <aperdomo@redhat.com>

--- projects/core/ci_entrypoint/run_common.py
+++ projects/core/ci_entrypoint/run_common.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 FORGE_HOME = Path(__file__).resolve().parent.parent.parent.parent
 
 EXTRA_PACKAGES = [
-    "benchconf @ git+https://github.com/openshift-psap/benchconf@fix/package-configs",
+    "benchconf @ git+https://github.com/openshift-psap/benchconf@main",
 ]
 
 # Global reference to child process for signal forwarding
